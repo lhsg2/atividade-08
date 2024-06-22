a0ac3f49e12f509e7498284a45464e48526fb744
Luiz Henrique <138501027+lhsg2@users.noreply.github.com>
2024-06-22 12:41:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+money = float(input("quanto é ganhado por hora:"))
+time = float(input("quantas horas trabalha por semana:"))
+bruto = money*(time*4)
+print(f"salario bruto:{bruto:.2f}")
+ir = bruto*0.11
+print(f"desconto de imposto de renda:{ir:.2f}")
+inss = bruto*0.08
+print(f"desconto de INSS:{inss:.2f}")
+sind = bruto*0.05
+print(f"desconto de sindicato:{sind:.2f}")
+liquido = bruto-(ir+inss+sind)
+print(f"salario liquido:{liquido}")
